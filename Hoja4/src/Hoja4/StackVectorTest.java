ac3ba5575634f1afe22584d1456d57c2384de5cd
lim14373 <lim14373@uvg.edu.gt>
2015-08-20 00:05:21 -0600

    JUnit

---
+++ Hoja4/src/Hoja4/StackVectorTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StackVector;
+
+import Calcu;
+import Calculadora;
+import AbstractStack;
+import Stack;
+import StackVector;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JavierLima
+ */
+public class StackVectorTest {
+    
+    private AbstractStack<Double> miStack = new StackVector<E>();//instanciamos un objeto de tipo Calcu para poder utilizar los metodos de la clase
+//esto se ejecuta al inicio del programa y permite declarar los estados iniciales de variables en comun de los metodos
+    @Before
+    public void push() {
+        miStack.push("1");
+        miStack.push("2");
+        miStack.push("3");
+    }
+//test para el metodo calcularVector
+    @Test
+    public void pop(){
+        String resultado = miStack.pop();
+        assertEquals("3",resultado); //compara el valor esperado y el valor resultante
+    }
+//test para el metodo getResultado
+    @Test
+    public void getResultado(){
+        int otro = miStack.size();
+        assertEquals(2,otro);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
